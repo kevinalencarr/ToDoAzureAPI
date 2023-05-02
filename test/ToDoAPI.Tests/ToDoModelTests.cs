209dfec15b4c174bdebf0a0f08648f73160d08bb
kevinalencarr <kevinribeiroalencar@gmail.com>
2023-05-02 17:23:13 -0300

    added model unit testd

---
+++ test/ToDoAPI.Tests/ToDoModelTests.cs
@@ -0,0 +1,22 @@
+using ToDoAPI.Models;
+
+namespace ToDoAPI.Tests;
+
+public class ToDoModelTests
+{
+    [Fact]
+    public void CanChangeName()
+    {
+        // Arrange
+        var testToDo = new ToDoItem
+        {
+            Name = "Complete unit testing"
+        };
+
+        // Act
+        testToDo.Name = "Complete Azure pipelines";
+
+        // Assert
+        Assert.Equal("Complete Azure pipelines", testToDo.Name);
+    }
+}
